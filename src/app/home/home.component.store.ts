8f66e7632f4caf0feb30e1da407e2148c37e32fb
Yaswanth Akula <yaswanthakula@Yaswanths-MacBook-Air.local>
2025-01-27 00:48:53 +0530

    implemented signalstore

--- src/app/home/home.component.store.ts
+++ src/app/home/home.component.store.ts
@@ -1,1 +1,40 @@
-import { signalStore, withState } from '@ngrx/signals';
+import { patchState, signalStore, withComputed, withMethods, withState } from '@ngrx/signals';
+import { rxMethod } from '@ngrx/signals/rxjs-interop';
+import { User } from '../../utils/models/user';
+import { computed, inject } from '@angular/core';
+import { UserService } from '../../services/users.service';
+import { pipe, switchMap, tap } from 'rxjs';
+import { tapResponse } from '@ngrx/operators';
+
+type userState = {
+    users: User[],
+    isLoading: boolean;
+};
+
+const INITIAL_STATE: userState = {
+    users: [],
+    isLoading: false
+};
+
+export const UsersStore = signalStore(
+    withState(INITIAL_STATE),
+    withComputed(({users}) => ({
+        isUsersEmpty: computed(() => users.length === 0)
+    })),
+    withMethods((store, userService = inject(UserService)) => ({
+        loadUsers: rxMethod(
+            pipe(
+                tap(() => patchState(store, {isLoading: true})),
+                switchMap(() => {
+                    return userService.getUsers().pipe(
+                        tapResponse({
+                            next: (users: User[]) => patchState(store, { users }),
+                            error: console.error,
+                            finalize: () => patchState(store, { isLoading: false }),
+                        })
+                    )
+                })
+            )
+        )
+    }))
+)
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import {Component} from "@angular/core";
 
 @Component({
-  selector: 'home',
-  templateUrl: './home.component.html',
-  styles: [``]
+    selector: 'home',
+    templateUrl: './home.component.html',
+    styles: [``],
+    standalone: false
 })
 export class HomeComponent {
 }
